65b302a1948e91486432a770ca27576c8097d49d
urmumsmain <79919422+MykeHaunt@users.noreply.github.com>
2025-04-04 01:02:15 +0600

    Create initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,4 @@
+mkdir -p "VIBRATION ANALYSIS FOR ENGINES LACKING OBD II"/{src,docs,examples,.github}
+cd "VIBRATION ANALYSIS FOR ENGINES LACKING OBD II"
+touch README.md LICENSE CONTRIBUTING.md Dockerfile docker-compose.yml
+mkdir -p src/{core,dashboard,models} examples/sample_data
