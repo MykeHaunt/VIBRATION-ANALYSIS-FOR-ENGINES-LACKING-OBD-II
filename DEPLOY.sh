10d9566ca1843f7c2ab9f3e97fb22ac271cfb231
urmumsmain <79919422+MykeHaunt@users.noreply.github.com>
2025-04-04 01:01:03 +0600

    Create DEPLOY.sh

---
+++ DEPLOY.sh
@@ -0,0 +1,6 @@
+# Start realtime dashboard (new terminal)
+python src/core/engine_analysis.py
+
+# In main terminal
+curl -LJO https://raw.githubusercontent.com/yourusername/repo/main/requirements.txt
+pip install -r requirements.txt
